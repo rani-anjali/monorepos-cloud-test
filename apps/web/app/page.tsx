68c4649eecb3b096b7262d970174bf9dee012b1d
Anjali Rani <105735270+rani-anjali@users.noreply.github.com>
2025-02-26 20:19:24 +0530

    Update page.tsx with label name and password.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -9,7 +9,9 @@ export default async function Home() {
   const user = await client.user.findFirst()
   return (
     <div>
+      name:
       {user?.username}
+      password:
       {user?.password}
     </div>
   );
